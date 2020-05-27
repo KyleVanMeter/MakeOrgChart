1b42d93661c324be924e7ebb68072886bafa1e3f
Kyle Van Meter <kvanmete76751@gmail.com>
2020-05-27 15:47:26 -0600

    Initial d3-graphviz
    Figure out how to get wasm to work with webpack

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,32 +1,33 @@
 <template>
-    <div class='circle'>
+    <div class='circle' ref='stuff'>
         <h1> Circles </h1>
         <h2> {{ msg }} </h2>
-    <svg
-    :height='height'
-    :width='width'
-    >
-    </svg>
+        <div id='graph'>
+        </div>
     </div>
 </template>
 
 <script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator'
 import * as d3 from 'd3'
+import * as graphviz from 'd3-graphviz'
 
 @Component
 export default class Chart extends Vue {
     private msg: string = 'Chart msg'
     private height: number = 500
     private width: number = 600
+    public getDim () {
+        const div: HTMLDivElement = this.$refs.stuff as HTMLDivElement
+        this.height = div.clientHeight
+        this.width = div.clientWidth
+    }
 
     mounted () {
-        d3.select('svg')
-        .append('circle')
-        .attr('r', 5)
-        .attr('cx', this.width / 3.0)
-        .attr('cy', this.height / 3.0)
-        .attr('fill', 'red')
+        this.getDim()
+        d3.select('#graph')
+        .graphviz()
+        .renderDot('digraph  {a -> b}')
     }
 }
 </script>
