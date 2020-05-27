4b329c7f85d3d2a550481e9d55ba28ba82500b13
Kyle Van Meter <kvanmete76751@gmail.com>
2020-05-27 17:40:52 -0600

    Add vue / webpack config nonsense for graphviz.wasm
    webpack is hellworld

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -10,7 +10,7 @@
 <script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator'
 import * as d3 from 'd3'
-import * as graphviz from 'd3-graphviz'
+import 'd3-graphviz'
 
 @Component
 export default class Chart extends Vue {
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  /*chainWebpack: webpackConfig => {
+    webpackConfig.module
+     .rule('wasm')
+       .test(/.wasm$/)
+       .use('wasm-loader')
+       .loader('wasm-loader')
+  }*/
+  configureWebpack: {
+      plugins: [
+          new CopyWebpackPlugin({
+            patterns: [{
+                from: 'node_modules/@hpcc-js/wasm/dist/graphvizlib.wasm',
+                to: path.resolve(__dirname, 'public/js')
+            }],
+        }),
+      ],
+  }
+}
