59c80b6a60e2fc383d82396d6983e56ddc5ed343
Kyle Van Meter <kvanmete76751@gmail.com>
2020-05-29 15:38:28 -0600

    Added graphlib / graphlib-dot, added delete method

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -8,6 +8,8 @@
             <button v-on:click="addEdgeEvent">Add Edge</button>
             <input v-model="toNode">
             <input v-model="fromNode">
+            <button v-on:click="deleteNodeEvent">Delete Node</button>
+            <input v-model="delNode">
         </div>
         <div id='graph'>
         </div>
@@ -18,7 +20,8 @@
 import { Component, Vue } from 'vue-property-decorator'
 import * as d3 from 'd3'
 import 'd3-graphviz'
-import { Node, Graph } from '../Graph'
+import { Graph } from 'graphlib'
+import * as dot from 'graphlib-dot'
 
 @Component
 export default class Chart extends Vue {
@@ -28,28 +31,41 @@ export default class Chart extends Vue {
     private nodeData: string = ''
     private toNode: string = ''
     private fromNode: string = ''
+    private delNode: string = ''
 
     private _graph: Graph = new Graph()
+
+    public deleteNodeEvent () {
+        this._graph.removeNode(this.delNode)
+        console.log('deleted!')
+
+        d3.select('#graph')
+        .graphviz()
+        .height(this.height)
+        .width(this.width)
+        .renderDot(dot.write(this._graph))
+    }
+
     public addNodeEvent () {
-        this._graph.insert(this.nodeData)
+        this._graph.setNode(this.nodeData)
         console.log(this._graph)
 
         d3.select('#graph')
         .graphviz()
         .height(this.height)
         .width(this.width)
-        .renderDot(this._graph.toDot())
+        .renderDot(dot.write(this._graph))
     }
 
     public addEdgeEvent () {
-        this._graph.insertDirectedEdge(new Node(this.fromNode), new Node(this.toNode))
+        this._graph.setEdge(this.toNode, this.fromNode)
         console.log(this._graph)
 
         d3.select('#graph')
         .graphviz()
         .height(this.height)
         .width(this.width)
-        .renderDot(this._graph.toDot())
+        .renderDot(dot.write(this._graph))
     }
 
     public getDim () {
