635620bd23db6318936c31921376f3dcfcec9c80
Kyle Van Meter <kvanmete76751@gmail.com>
2020-06-05 18:49:05 -0600

    Added simple single-col HTML table builder util func

--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,24 @@ export interface HTMLMap {
     [nodeIndex: string]: string
 }
 
-function HTMLTableBuilder<T> (numRows: number, numCols: number, data: Array<T>): string {
+function HTMLWrapper (inner: string): string {
+    return ` [shape=plaintext, label=<\n${inner}>]`
+}
+
+export function HTMLListBuilder<T extends any> (data: Array<T>): string {
+    let arrString: string = `<table>\n`
+    let numRows = data.length
+
+    for (let n = 0; n < numRows; n++) {
+        arrString +=
+        `  <tr>\n    <td> ${data[n]} </td>\n  </tr>\n`
+    }
+    arrString += `</table>`
+
+    return arrString
+}
+
+export function HTMLTableBuilder<T> (numRows: number, numCols: number, data: Array<T>): string {
     let arrString: string = `<table>\n`
     for (let n = 0; n < numRows; n++) {
         arrString += `<tr>\n`
